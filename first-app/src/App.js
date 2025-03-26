09fc0d8d57a5f557f342bc8cdea1329f064d3e28
Your Name <you@example.com>
2025-03-26 23:03:38 +0300

    current

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Fcomponents from './Fcomponents';
 import Ccomponent from './Ccomponent';
-import Menu from './Menu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Fcomponents name="Alex" />
-        <Ccomponent numbers={[1, 2, 3]} />
-        <Menu />
+
+        <Ccomponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Web developer blog
